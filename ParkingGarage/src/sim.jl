8923f1c2a8d40f015f4b2e601c7d817d58f20ea7
jonagan1 <156458400+jonagan1@users.noreply.github.com>
2024-03-21 15:02:10 -0500

    Update sim.jl

--- ParkingGarage/src/sim.jl
+++ ParkingGarage/src/sim.jl
@@ -34,6 +34,7 @@ If we are following the adaptive policy, then the rule is slightly more complica
 We add `n_levels` in the first year. Then, every future year we compare the capacity and demand. If the demand is greater than the capacity, we add a level.
 """
 function get_action(x::ParkingGarageState, policy::AdaptivePolicy)
+    capacity = calculate_capacity(x)
     if x.year == 1
         return ParkingGarageAction(policy.n_levels_init)
     elseif x.demand > capacity
